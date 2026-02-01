6fcf4009cb8a12b7ff6c29f6c5ac3b2a1923787c
jassini <jasin.kfa1@gmail.com>
2026-02-02 00:31:02 +0100

    Rregullim i vitit në footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <?php
 ?>
 <footer class="main-footer">
-    <p>© 2025 ExploreKosova • Të gjitha të drejtat e rezervuara</p>
+    <p>© 2026 ExploreKosova • Të gjitha të drejtat e rezervuara</p>
 </footer>
 
 <script src="assets/js/main.js"></script>
